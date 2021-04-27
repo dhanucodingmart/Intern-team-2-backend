011e829dc1ca0f034d94d3bce15267a8e2446379
Sethu-1471 <rpsethu1471@gmail.com>
2021-04-27 10:30:18 +0530

    Initial commit

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ app.use(cors());
 app.use('/auth', require('./routes/auth'));
 
 //PORT
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("Running on port", PORT, "mode", process.env.NODE_ENV));
