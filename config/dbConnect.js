eba67cc2e46bd4c481582276bc9c151797317a64
Sethu-1471 <rpsethu1471@gmail.com>
2021-04-27 10:37:47 +0530

    Updated

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose")
-let MONGO_DB_URI_REMOTE = "mongodb + srv://sethu:sethu*1471@mycluster.sdopd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 const connectDB = async () => {
     try {
-        const con = await mongoose.connect('mongodb + srv://sethu:sethu*1471@mycluster.sdopd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+        let MONGO_DB_URI_REMOTE = "mongodb+srv://sethu:sethu*1471@mycluster.sdopd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        const con = await mongoose.connect(MONGO_DB_URI_REMOTE, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
